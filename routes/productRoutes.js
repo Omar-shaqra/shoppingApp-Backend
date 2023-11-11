a6c4e386993fb83ca3981a19d1f0363ec8c08461
Omar-shaqra <49815667+Omar-shaqra@users.noreply.github.com>
2023-11-11 18:58:39 +0200

    Create productRoutes.js

---
+++ routes/productRoutes.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+const productsClass = require("../controllers/productservices");
+const productsObj = new productsClass();
+
+const Authservices = require("../controllers/authservices");
+
+//mergeparams : allow us to access the parameters on other routes
+const router = express.Router({ mergeParams: true });
+
+const {
+  createProductValidator,
+  getProductValidator,
+  updateProductValidator,
+  deleteProductValidator,
+} = require("../utils/validator/productvalidation");
+
+//router.get('/',catigoriesObj.createCategory);
+router
+  .route("/")
+  .post(
+    // Authservices.protect,
+    // Authservices.allowedto("admin", "manager"),
+    // productsObj.uploadProductImages,
+    // productsObj.resizeProductImages,
+    createProductValidator,
+    productsObj.createproduct
+  )
+  .get(productsObj.getproducts);
+router
+  .route("/:id")
+  .get(getProductValidator, productsObj.getoneproduct)
+  .put(
+    Authservices.protect,
+    Authservices.allowedto("admin", "manager"),
+    productsObj.uploadProductImages,
+    productsObj.resizeProductImages,
+    updateProductValidator,
+    productsObj.updateproduct
+  )
+  .delete(
+    Authservices.protect,
+    Authservices.allowedto("admin"),
+    deleteProductValidator,
+    productsObj.deleteproduct
+  );
+
+module.exports = router;
