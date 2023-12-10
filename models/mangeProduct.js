70f938168bf5fda95b375c256cc69848e3279f47
Tarek <123814096+Tarek018@users.noreply.github.com>
2023-12-10 14:34:28 +0000

    Update mangeProduct.js

---
+++ models/mangeProduct.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const manageProduct = new mongoose.Schema(
+  {
+    pdoductId: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Product",
+      required: true,
+    },
+    storeId:{
+        type: mongoose.Schema.ObjectId,
+        ref: "Store",
+        required: true,
+    },
+    userId:{
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: true,
+    },
+    date: {
+        type: Date,
+        required: true
+    }
+  },
+  { timestamps: true }
+);
+
+
+
+module.exports = mongoose.model("manageProduct", manageProduct);
