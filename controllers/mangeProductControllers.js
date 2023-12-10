3837ceb69e21e225121cd16f6d67dc6b1c17aae9
Tarek <123814096+Tarek018@users.noreply.github.com>
2023-12-10 18:20:10 +0000

    Update mangeProductControllers.js

--- controllers/mangeProductControllers.js
+++ controllers/mangeProductControllers.js
@@ -5,7 +5,6 @@ const manageProduct = require("../models/manageProductModel");
 //d@desc create manage product
 //@route post /manageProduct
 const create = async(req, res) => {
-    console.log(req.body);
     const manageProducte = await manageProduct.create(req.body);
     res.status(200).json({ data: manageProducte });
 };
