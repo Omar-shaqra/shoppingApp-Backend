57c0f6c3cfacd57e46b71e3f5ecc941ed12cfc36
Tarek <123814096+Tarek018@users.noreply.github.com>
2023-12-10 18:41:43 +0000

    Update mangeProductControllers.js

--- controllers/mangeProductControllers.js
+++ controllers/mangeProductControllers.js
@@ -1,12 +1,12 @@
 
 const manageProduct = require("../models/manageProductModel");
 
-const create = async(req, res) => {
+const create = async (req, res) => {
     const manageProducte = await manageProduct.create(req.body);
     res.status(200).json({ data: manageProducte });
 };
 
-const getByStoreId = async(req, res) => {
+const getByStoreId = async (req, res) => {
     const { id } = req.params;
     await manageProduct.find({shopeId: {$eq:id} })
         .then((docs)=>{
@@ -16,6 +16,10 @@ const getByStoreId = async(req, res) => {
             res.status(404),json({ error: error.message});
         });
 };
+
+const deleteOne = async (req, res) => {
+    
+}
 
 module.exports = {
     create,
