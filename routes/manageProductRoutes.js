57ffcd5bbf975e0ddda1b6acdb0c5965ed7e9857
Tarek <123814096+Tarek018@users.noreply.github.com>
2023-12-10 19:43:28 +0000

    Update manageProductRoutes.js

--- routes/manageProductRoutes.js
+++ routes/manageProductRoutes.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const { create, getByStoreId, deleteOne } = require("../controllers/mangeProductControllers");
-const router = express.Router();
 const { manageProductValidation } = require("../utils/validator/manageProductValidator");
 
+const router = express.Router();
 
 router.post('/', manageProductValidation, create);
 
 router.get('/:id', getByStoreId);
 
 router.delete('/:id', deleteOne);
-
 
 module.exports = router;
